d428afa196339e3925b6f031dce23773b66829d2
vah_13 <vvvaagn@gmail.com>
2017-08-01 12:14:32 +0300

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -15,9 +15,10 @@ using namespace std;
 
 list<int> optimize(int j_arr[],int j_len)
 {		
+	cout << "start optimize\n";
 list<int> tmp_arr;
 
-	for (int j=0;j<j_len;j+=2)
+	for (int j=0;j<j_len-1;j+=2)
 		{
 
 			if(j_arr[j+1]-j_arr[j]<=3)
@@ -104,21 +105,31 @@ bool readMemory(DWORD pid, char* _p, size_t len)
 		chunk.resize(len);
 		SIZE_T bytesRead;
 		ReadProcessMemory(process, p, &chunk[0], len, &bytesRead);
+		
+		if (bytesRead<6*2)
+		{
+			return false;
+		}
 		for (int i = 0;i<bytesRead;i+=2)
 		{
-			if (!isalnum(chunk[i]) 	&& 	!isprint(chunk[i]))
+			if (!isprint(chunk[i]) || ((int)chunk[i])<=0)
 			{
 					return false;
 			}
 		}
-
-		cout<<"***********"<<endl<<intToHexString((int)(void*)p)<<endl<<"Password: ";		
-		for (i = 0;i<bytesRead;i+=2)
+		
+		//cout<< "length = " << bytesRead << endl;
+		
+		if (bytesRead>1)
 		{
-                cout<< chunk[i];	
+			//cout<<"***********"<<endl<<intToHexString((int)(void*)p)<<endl<<"Password: ";		
+			for (i = 0;i<bytesRead;i+=2)
+			{
+					cout<< /*"char = "<< */chunk[i] ;/*<< " int = " << (int)chunk[i];*/	
+			}
+			cout <<endl;
+			//cout<<intToHexString((int)(void*)p)<<endl<<"***********"<<endl;
 		}
-		cout <<endl;
-		cout<<intToHexString((int)(void*)p)<<endl<<"***********"<<endl;
 		
     }
 	return true;
@@ -126,9 +137,11 @@ bool readMemory(DWORD pid, char* _p, size_t len)
 
 void foo(const std::list<int>& first, const std::list<int>& second, const int limit)
 {
+	cout << "foo\n";
     std::list<int>::const_iterator it1 = first.begin();
     std::list<int>::const_iterator  it2 = second.begin();
 
+	
     while (it1 != first.end() && it2 != second.end()) {
         if (*it1 + limit < *it2) {
             ++it1;
@@ -136,19 +149,13 @@ void foo(const std::list<int>& first, const std::list<int>& second, const int li
             ++it2;
         } else {
 
-		//	if (
-//			readMemory(7540,(char*)(void*)(*it1+2),*it2-*it1-2);//);
-//		//					{	
-							//	break;
-		//					}
 					
             //std::cout << *it2 <<" "<< *it1 << std::endl;
-			//if (*it2 > *it1)
 			{
-				            std::cout << intToHexString(*it1+2) <<" "<< intToHexString(*it2) <</*" "/*<<*it2-*it1<< */std::endl;
+				            //std::cout << intToHexString(*it1+2) <<" "<< intToHexString(*it2) << " " <<*it2-*it1<< std::endl;
 							try{
-								if (*it1+2<*it2)
-							readMemory(7540,(char*)(void*)(*it1+2),*it2-*it1);//);
+								if (*it2-*it1>0)
+									readMemory(6604,(char*)(void*)(*it1+2),*it2-*it1-2);//);
 							}catch (int a){
 							cout<< "cc"<<endl;
 							}
@@ -158,9 +165,9 @@ void foo(const std::list<int>& first, const std::list<int>& second, const int li
     }
 }
 
-list<int> GetAddressOfData(DWORD pid, const char *data, size_t len)
+void GetAddressOfData(DWORD pid, const char *data, size_t len, list<int>& entries)
 {
-	list<int> entries;
+	//list<int>* entries;
 	int count = 0;
     HANDLE process = OpenProcess(PROCESS_VM_READ | PROCESS_QUERY_INFORMATION, FALSE, pid);
     if(process)
@@ -199,75 +206,100 @@ list<int> GetAddressOfData(DWORD pid, const char *data, size_t len)
 
         }
     }
+	
 	cout << "count " <<  count << '\n';
-    return entries;
+	cout << "start return\n";
+		
+	return ;
 }
 
 void __main(bool flag)
 {
 		const char start_magic_data[] = "\x00\x88"; 
-  		char _end_magic_data1[] = "\x00\x00\x00\x00\x00\x00";
-		char _end_magic_data2[] = "\x00\x00\x00\x00\x00\x00\x00";
+  		char _end_magic_data1[] = "\x00\x00\x00\x20\x00\x00";
+		char _end_magic_data2[] = "\x00\x00\x00\x00\x00\x00";
 		if (flag)
 		{
-			//*_end_magic_data1 = *_end_magic_data1;
+			*_end_magic_data1 = *_end_magic_data1;
 		}
 		else
 		{
-			//memcpy(_end_magic_data1, _end_magic_data2, sizeof(_end_magic_data2));
+			memcpy(_end_magic_data1, _end_magic_data2, sizeof(_end_magic_data2));
 		}
 
+		for (int mg=0;mg<6;mg++)
+		{
+			cout<< (int)_end_magic_data1[mg];
+		}
 		int PID = GetProcId("TeamViewer.exe");	
 		std::cout << "Local data address: " << (void*)start_magic_data << "\n";
-		list<int> start_address = GetAddressOfData(PID, start_magic_data, 2);	
 		cout<<"start end magic search "<<'\n';	
-		list<int> end_address = GetAddressOfData(PID, _end_magic_data1, (sizeof(_end_magic_data1)/sizeof(*_end_magic_data1))-1);
+		
+		
+		list<int> start_address ; 
+			GetAddressOfData(PID, start_magic_data, 2, start_address);	
+		cout << "DDDDDDDDDD " << std::distance(start_address.begin(),start_address.end()) << endl;
+
+
+		list<int> end_address  ; 
+			GetAddressOfData(PID, _end_magic_data1, (sizeof(_end_magic_data1)/sizeof(*_end_magic_data1))-1,end_address );
+		cout << "DDDDDDDDDD " << std::distance(end_address.begin(),end_address.end()) << endl;
+		
+		
+		
+		//start_address.reverse();
+	//	end_address.reverse();
+
+
 		int key = 0;
 		int count = 0;
 		
-		
+
 		/* i - index of start_address
 			std::vector<double> v;
 			double* i_arr = &v[0];
 		*/
 
-		int i_len = std::distance(start_address.begin(),start_address.end()); 
-		int *i_arr = new int[i_len];
-		std::copy(start_address.begin(), start_address.end(), i_arr);
+		//int i_len = std::distance(start_address.begin(),start_address.end()); 
+		//int *i_arr = new int[i_len];
+		//std::copy(start_address.begin(), start_address.end(), i_arr);
 		
 
-		int j_len = std::distance(end_address.begin(),end_address.end()); 
-		int *j_arr = new int[j_len];;
-		std::copy(end_address.begin(), end_address.end(), j_arr);
+		//int j_len = std::distance(end_address.begin(),end_address.end()); 
+		//int *j_arr = new int[j_len];;
+		//std::copy(end_address.begin(), end_address.end(), j_arr);
 		//optimize
-		list<int> ret_list ;
+		//list<int> ret_list ;
 		
-		cout << j_len << endl;
+		//cout << j_len << endl;
 		
+
 		//ret_list = optimize(i_arr,i_len);
 		//start_address.clear();
 		//start_address = ret_list;
-		cout << "leeength " << std::distance(start_address.begin(),start_address.end())<<endl;
+		//cout << "leeength " << std::distance(start_address.begin(),start_address.end())<<endl;
 
 		//ret_list = optimize(j_arr,j_len);
 		//end_address.clear();
 		//end_address = ret_list;
-		cout << "leeength " << std::distance(end_address.begin(),end_address.end())<<endl;
+		//cout << "leeength " << std::distance(end_address.begin(),end_address.end())<<endl;
+
+		cout << "_main foo\n";
 
 		foo(start_address,end_address,33);
 		cout<< "ENDDD "<< endl;
 		cin.get();
-		ret_list.clear();		
-		start_address.reverse();
-		end_address.reverse();
+		///ret_list.clear();		
+		//start_address.reverse();
+		//end_address.reverse();
 
 
-		cout << intToHexString(i_arr[0]) << " " ;
-
+//		cout << intToHexString(i_arr[0]) << " " ;
+/*
 		for (std::list<int>::const_iterator iterator = start_address.begin(), end = start_address.end(); iterator != end; ++iterator) 
 		{
 			int i = *iterator;
-			cout << i << endl;
+			
 			key = i;	
 			for (int j=key;j<key+33;j++)
 			{
@@ -285,7 +317,7 @@ void __main(bool flag)
 						//cout << count << endl;
 		}
 
-	cout<<count;
+	cout<<count;*/
 }
 int main()
 {
